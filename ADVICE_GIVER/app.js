2e660f14c57938d848cc1dd4c29a2e8b8449666e
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-11-07 21:13:27 +0000

    davamm

--- ADVICE_GIVER/app.js
+++ ADVICE_GIVER/app.js
@@ -1,4 +1,9 @@
 "use strict";
 $(document).ready(function () {
+    https://www.trendyol.com/mancy/kadin-siyah-baskili-kapusonlu-sweatshirt-p-200345156?boutiqueId=615610&merchantId=434441&sav=true
+
+
+
+
 
 });

fb6498e2b06f6a24d2dd363ad423e4e773c8372c
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-11-13 16:56:54 +0000

    davam

--- ADVICE_GIVER/app.js
+++ ADVICE_GIVER/app.js
@@ -1,13 +1,15 @@
+const advicenumber = document.querySelector(".advicenumber")
 const advice = document.querySelector(".advice")
 const btn = document.querySelector(".submit")
-
 
 btn.addEventListener('click', function (name) {
     let url = "https://api.adviceslip.com/advice"
     fetch(url)
-    .then(response => response.json())
-    .then(data =>{
-        console.log(data.slip.id);
-        console.log(data.slip.advice);
-    })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data.slip.id);
+            // console.log(data.slip.advice);
+            advicenumber.innerHTML = data.slip.id
+            advice.innerHTML = data.slip.advice
+        })
 })
